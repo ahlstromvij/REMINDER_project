cc0a68c625205178568f4f4e8d30e3163ea5394d
Kristoffer Ahlstrom-Vij <ahlstromvij@gmail.com>
2022-01-05 22:14:57 +0000

    Added immigration variables

--- preprocessing.R
+++ preprocessing.R
@@ -1,15 +1,15 @@
-# packages
+# load packages
 library(haven)
 
-# read in data
+# read in SPSS data
 all_data <- read_spss("10085_da_en_v1_0.zsav")
 
-# demographic variables
+# add demographic variables
 df <- data.frame(all_data$W1_Q1) # 1 = man; 2 = woman
-names(df)[1] <- "gender"
+names(df)[ncol(df)] <- "gender"
 
 df <- cbind(df, all_data$W1_Q2A) # continuous version; see Q2C for ordinal
-names(df)[2] <- "age"
+names(df)[ncol(df)] <- "age"
 
 df$nationality <- NA
 for(i in 1:length(df$nationality)){
@@ -41,14 +41,77 @@ for(i in 1:length(df$nationality)){
 table(df$nationality)
 
 df <- cbind(df, all_data$W1_Q40) # see https://ec.europa.eu/eurostat/statistics-explained/index.php?title=International_Standard_Classification_of_Education_(ISCED)
-names(df)[4] <- "education_ISCED"
+names(df)[ncol(df)] <- "education_ISCED"
 
 df <- cbind(df, all_data$W1_Q45) # 1 = in paid work
-names(df)[5] <- "in_paid_work"
+names(df)[ncol(df)] <- "in_paid_work"
 
 df <- cbind(df, all_data$W2_Q87) 
-names(df)[6] <- "religion" # lots of NAs
+names(df)[ncol(df)] <- "religion" # lots of NAs
 
-# attitudinal variables
+# ideology
 df <- cbind(df, all_data$W1_Q8) 
-names(df)[7] <- "ideology_left_right" # 1 = max left; 10 = max right
+names(df)[ncol(df)] <- "ideology_left_right" # 1 = max left; 10 = max right
+
+# general immigration attitudes
+# How do you feel about immigration by the following groups of people to [COUNTRY]?
+df <- cbind(df, all_data$W1_Q15_1) 
+names(df)[ncol(df)] <- "gen_imm_escape" # Those who come to escape war and oppression
+# 1 = very negatively; 5 = very positively
+
+df <- cbind(df, all_data$W1_Q15_2) 
+names(df)[ncol(df)] <- "gen_imm_work" # Those who come to work
+# 1 = very negatively; 5 = very positively
+
+df <- cbind(df, all_data$W1_Q15_3) 
+names(df)[ncol(df)] <- "gen_imm_study" # Those who come to study
+# 1 = very negatively; 5 = very positively
+
+df <- cbind(df, all_data$W1_Q15_4) 
+names(df)[ncol(df)] <- "gen_imm_family_war" # Those who come to live with their family after separation, due to war and oppression
+# 1 = very negatively; 5 = very positively
+
+df <- cbind(df, all_data$W1_Q15_5) 
+names(df)[ncol(df)] <- "gen_imm_marriage" # Those who come to live with their family, due to marriage or one spouse getting a job in [COUNTRY]
+# 1 = very negatively; 5 = very positively
+
+# immigration items from ESS
+df <- cbind(df, all_data$W1_Q16) 
+names(df)[ncol(df)] <- "gen_imm_ess_good_bad" # Would you say it is generally bad or good for [country]'s economy that people come to live here from other countries?
+# 0 = bad; 10 = good
+
+df <- cbind(df, all_data$W1_Q17) 
+names(df)[ncol(df)] <- "gen_imm_ess_jobs" # Would you say that people who come to live here generally take jobs away from workers in [country], or generally help to create new jobs
+# 0 = take jobs; 10 = create new jobs
+
+df <- cbind(df, all_data$W1_Q18) 
+names(df)[ncol(df)] <- "gen_imm_ess_welfare" # Most people who come to live here work and pay taxes. They also use health and welfare services. On balance, do you think people who come here take out more than they put in or put in more than they take out?
+# 0 = take out more; 10 = put in more
+
+df <- cbind(df, all_data$W1_Q19) 
+names(df)[ncol(df)] <- "gen_imm_ess_safety" # Is [country]’s safety situation made better or worse by people coming to live here from other countries?
+# 0 = made better; 10 = made worse
+
+# emotions about immigration
+# 1 = not at all; 7 = a lot
+df <- cbind(df, all_data$W1_Q20_1)
+names(df)[ncol(df)] <- "imm_anger" 
+df <- cbind(df, all_data$W1_Q20_2)
+names(df)[ncol(df)] <- "imm_fear" 
+df <- cbind(df, all_data$W1_Q20_3)
+names(df)[ncol(df)] <- "imm_hope" 
+df <- cbind(df, all_data$W1_Q20_4)
+names(df)[ncol(df)] <- "imm_sympathy" 
+
+# estimations of size of immigrant group
+df <- cbind(df, all_data$W1_Q25_2)
+names(df)[ncol(df)] <- "imm_size" # Out of every 100 people in [COUNTRY], how many do you think were born outside [COUNTRY]? (percentage)
+
+# free movement
+df <- cbind(df, all_data$W1_Q28_3)
+names(df)[ncol(df)] <- "free_move_protect_jobs" # The movement of individuals between EU countries should be restricted to protect native workers.
+# 1 = strongly disagree; 5 = strongly agree
+
+df <- cbind(df, all_data$W1_Q28_4)
+names(df)[ncol(df)] <- "free_move_protect_services" # The movement of individuals between EU countries should be restricted to avoid exploitation of the public services (e.g. education and health services).
+# 1 = strongly disagree; 5 = strongly agree
