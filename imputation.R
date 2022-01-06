0a1cf6b1da668e28c61f08bc5abfcfa8fa80fd7a
Kristoffer Ahlstrom-Vij <ahlstromvij@gmail.com>
2022-01-06 09:58:21 +0000

    Missingness by wave in imputation script

--- imputation.R
+++ imputation.R
@@ -1,7 +1,15 @@
 # read in preprocessed data
 df_preprocessed <- read.csv("df_preprocessed.csv")
 
-# visualise missing data
-library(naniar)
-vis_miss(df_preprocessed[,c(1:30,36:38)]) # ignoring quality control NAs
+# observations by country
+table("Country" = df_preprocessed$nationality)
+table("Country" = df_preprocessed$nationality,
+      "Took part in W2" = df_preprocessed$part_in_W2)
 
+# visualize missing data
+library(naniar)
+vis_miss(df_preprocessed[,c(1:30)]) # ignoring quality control, weights, and W2 flag
+
+# only those who also took part in W2
+df_W2 <- subset(df_preprocessed, part_in_W2==1)
+vis_miss(df_W2[,c(1:30)]) # ignoring quality control, weights, and W2 flag
