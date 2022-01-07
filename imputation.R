1aea46e4bca345580f24d6b6b95d46448e17a718
William Allen <william.l.allen@gmail.com>
2022-01-07 09:53:30 +0000

    Added tabular summary of Wave 2 data

--- imputation.R
+++ imputation.R
@@ -13,3 +13,8 @@ vis_miss(df_preprocessed[,c(1:30)]) # ignoring quality control, weights, and W2 
 # only those who also took part in W2
 df_W2 <- subset(df_preprocessed, part_in_W2==1)
 vis_miss(df_W2[,c(1:30)]) # ignoring quality control, weights, and W2 flag
+
+# summarize wave 2 data
+library(modelsummary)
+datasummary_skim(df_W2) # numerical variables
+datasummary_skim(df_W2, type="categorical") # categorical variables
